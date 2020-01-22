c215fe4a9624817207ac5e37a72d142b5a6b126b
jakeb11499@gmail.com <jakeb11499@gmail.com>
2020-01-22 08:20:22 -0700

    First version of Say My Name

---
+++ Say_My_Name/Module1.vb
@@ -0,0 +1,20 @@
+﻿'Jake Burrell
+'RCET0265
+'Spring 2020
+'Say My Name
+'https://github.com/burrjake/JDB-VS-S20
+
+Option Strict On
+Option Explicit On
+Module Module1
+
+    Sub Main()
+        Dim userMessage As String
+
+        Console.WriteLine("Hello, what is your name?")
+        userMessage = Console.ReadLine()
+        Console.WriteLine("Hello, " & userMessage & " when you are done press enter.")
+        Console.ReadLine()
+    End Sub
+
+End Module
