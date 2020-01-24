c6fb58529b64d8fd58049f0b2aeb160b70deef2a
jakeb11499@gmail.com <jakeb11499@gmail.com>
2020-01-24 08:36:24 -0700

    Add If statement so that only myself, Emily, and Joe are greeted

--- Say_My_Name/Module1.vb
+++ Say_My_Name/Module1.vb
@@ -6,17 +6,27 @@
 
 Option Strict On
 Option Explicit On
+Option Compare Text
 Module Module1
 
     Sub Main()
         'ask user "What's your name?"
-        'then display the message 
+        'then display the message for the appropriate user 
 
         Dim userMessage As String
 
         Console.WriteLine("What's your name?")
         userMessage = Console.ReadLine()
-        Console.WriteLine("Hello, " & userMessage & " when you are done press enter.")
+        If userMessage = "Jake" Then
+            Console.WriteLine("Welcome back, creator.")
+        ElseIf userMessage = "Emily" Then
+            Console.WriteLine("Hello, Emily when you are done press enter.")
+        ElseIf userMessage = "Joe" Then
+            Console.WriteLine("Hello, Joe when you are done press enter.")
+        Else
+            Console.WriteLine("Sorry, I do not recognize you. Press enter when you are done.")
+        End If
+
         Console.ReadLine()
     End Sub
 
