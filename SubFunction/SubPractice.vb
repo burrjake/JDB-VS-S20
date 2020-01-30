5ee35eea6f92588c36c8c6551192b8fe400f9f47
jakeb11499@gmail.com <jakeb11499@gmail.com>
2020-01-30 09:00:03 -0700

    Create Sub and Function

---
+++ SubFunction/SubPractice.vb
@@ -0,0 +1,22 @@
+﻿Option Strict On
+Option Explicit On
+
+Module SubPractice
+
+    Sub Main()
+        Dim number As Integer
+        number = 7
+        number += 2
+        Console.WriteLine("In Sub Main")
+        MySub()
+        MyFunc()
+    End Sub
+
+    Sub MySub()
+        Console.WriteLine("In Sub Main")
+    End Sub
+
+    Function MyFunc() As Integer
+        Console.WriteLine("In My Fun")
+    End Function
+End Module
