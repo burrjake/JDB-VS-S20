d75d474c821a5822cf049ad784a6b965dc593886
jakeb11499@gmail.com <jakeb11499@gmail.com>
2020-02-09 23:06:29 -0700

    Fix issue with function option not repeating if a incorrect command was entered

--- BetterCalculator/BetterCalculator.vb
+++ BetterCalculator/BetterCalculator.vb
@@ -17,40 +17,71 @@ Module BetterCalculator
         Dim promptUser As Boolean
         Dim userInput As String
 
+        'greet user and tell user how to quit
         promptUser = True
         quitProgram = False
         Do While quitProgram = False
             Console.WriteLine("Please enter two numbers. Enter " & Chr(34) & "Q" & Chr(34) & " at any time to quit.")
 
+            'prompt user to select two numbers
             Console.WriteLine("Choose a number")
-                While promptUser = True
-                    Try
+            promptUser = True
+            While promptUser = True
+                userInput = Console.ReadLine()
+                If userInput = "q" Then
                     promptUser = False
-                    userInput = Console.ReadLine()
-                    firstNumber = CInt(userInput)
-                    Console.WriteLine("You entered " & Chr(34) & firstNumber & Chr(34))
-                Catch ex As Exception
-                    Console.WriteLine("You entered " & Chr(34) & userInput & Chr(34) & ", please enter a whole number.")
-                    promptUser = True
+                    quitProgram = True
+                    Console.WriteLine("You entered " & Chr(34) & "q" & Chr(34))
+                    Console.WriteLine("Have a nice day")
+                    Console.WriteLine("Press enter to close this window")
+                    Console.ReadLine()
+                    End
+
+                    'display first number or prompt to enter a whole number
+                Else
+                    Try
+                        promptUser = False
+                        firstNumber = CInt(userInput)
+                        Console.WriteLine("You entered " & Chr(34) & firstNumber & Chr(34))
+                    Catch ex As Exception
+                        Console.WriteLine("You entered " & Chr(34) & userInput & Chr(34) & ", please enter a whole number.")
+                        promptUser = True
                     End Try
+                End If
             End While
             promptUser = True
 
+            '
             Console.WriteLine("Choose a number")
-                promptUser = True
-                While promptUser = True
+            promptUser = True
+            While promptUser = True
+                userInput = Console.ReadLine()
+
+                'perform quit
+                If userInput = "q" Then
+                    quitProgram = True
+                    Console.WriteLine("You entered " & Chr(34) & "q" & Chr(34))
+                    Console.WriteLine("Have a nice day")
+                    Console.WriteLine("Press enter to close this window")
+                    Console.ReadLine()
+                    End
+
+                    'display second number or prompt to enter a whole number
+                Else
                     Try
-                    promptUser = False
-                    userInput = Console.ReadLine()
-                    secondNumber = CInt(userInput)
-                    Console.WriteLine("You entered " & Chr(34) & firstNumber & Chr(34))
+                        promptUser = False
+                        secondNumber = CInt(userInput)
+                        Console.WriteLine("You entered " & Chr(34) & secondNumber & Chr(34))
                     Catch ex As Exception
-                    Console.WriteLine("You entered " & Chr(34) & userInput & Chr(34) & ", please enter a whole number.")
-                    promptUser = True
+                        Console.WriteLine("You entered " & Chr(34) & userInput & Chr(34) & ", please enter a whole number.")
+                        promptUser = True
                     End Try
-                End While
+                End If
+            End While
             promptUser = True
 
+            'promp user to select what math function is wanted
+retryOptions:
             Console.WriteLine("Choose one Of the following options:")
                 Console.WriteLine("1. Add")
                 Console.WriteLine("2. Subtract")
@@ -58,7 +89,17 @@ Module BetterCalculator
                 Console.WriteLine("4. Divide")
                 userMessage = Console.ReadLine()
 
-                If userMessage = "1" Then
+            'perform quit
+            If userMessage = "q" Then
+                quitProgram = True
+                Console.WriteLine("You entered " & Chr(34) & "q" & Chr(34))
+                Console.WriteLine("Have a nice day")
+                Console.WriteLine("Press enter to close this window")
+                Console.ReadLine()
+                End
+
+                'perform proper operation
+            ElseIf userMessage = "1" Then
                 Console.WriteLine(firstNumber & "+" & secondNumber & "=" & (firstNumber) + (secondNumber))
             ElseIf userMessage = "2" Then
                 Console.WriteLine(firstNumber & "-" & secondNumber & "=" & (firstNumber) - (secondNumber))
@@ -67,9 +108,10 @@ Module BetterCalculator
             ElseIf userMessage = "4" Then
                 Console.WriteLine(firstNumber & "/" & secondNumber & "=" & (firstNumber) / (secondNumber))
             Else
-                    Console.WriteLine("You entered" & Chr(34) & userMessage & Chr(34))
-                End If
-            Loop
+                Console.WriteLine("You entered " & Chr(34) & userMessage & Chr(34))
+                GoTo retryOptions
+            End If
+        Loop
 
 
     End Sub
