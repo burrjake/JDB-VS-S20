73d0e227e713826f88d0b1d6494185ed19311fe4
jakeb11499@gmail.com <jakeb11499@gmail.com>
2020-02-26 08:56:10 -0700

    Create Message Function

---
+++ MessageFunction/MessageFunction.vb
@@ -0,0 +1,20 @@
+﻿Module MessageFunction
+
+    Sub Main()
+        UserMessages(True, "Hello 1", False)
+        UserMessages(True, "Hello 2", False)
+        UserMessages(True, "Hello 3", False)
+        MsgBox(UserMessages(True, "", False))
+
+    End Sub
+
+    Function UserMessages(addMessage As Boolean, message As String, clearMessage As Boolean) As String
+        Static formattedMessages As String '= " "
+
+        formattedMessages &= message & vbNewLine
+
+        Return formattedMessages
+
+    End Function
+
+End Module
