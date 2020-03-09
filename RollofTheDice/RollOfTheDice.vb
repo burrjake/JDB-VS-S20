31889081f3f9f136879c1450c658f7a5f1f5b32f
jakeb11499@gmail.com <jakeb11499@gmail.com>
2020-03-09 09:14:47 -0600

    Create the first and second roll and their sum

--- RollofTheDice/RollOfTheDice.vb
+++ RollofTheDice/RollOfTheDice.vb
@@ -3,5 +3,22 @@ Imports System
 Module RollOfTheDice
     Sub Main()
 
+        Dim firstRoll As Integer
+        Dim secondRoll As Integer
+        Dim rollSum As Integer
+        'Dim diceArray(3, 1000) As Integer
+        'For i = 1 To 1000
+
+        Randomize()
+            firstRoll = ((Rnd() * 6) + 1)
+            Console.WriteLine(firstRoll)
+            Randomize()
+            secondRoll = ((Rnd() * 6) + 1)
+        Console.WriteLine(secondRoll)
+        rollSum = firstRoll + secondRoll
+            Console.WriteLine(rollSum)
+            Console.ReadLine()
+
+        'Next
     End Sub
 End Module
