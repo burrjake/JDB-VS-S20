ce2882a829d772e00d4b9ba0b9fd135466aea06b
jakeb11499@gmail.com <jakeb11499@gmail.com>
2020-03-10 09:19:25 -0600

    Add comments

--- RollofTheDice/RollOfTheDice.vb
+++ RollofTheDice/RollOfTheDice.vb
@@ -8,11 +8,18 @@ Module RollOfTheDice
         Dim diceArray(12, 2) As Integer
         For i = 1 To 1000
 
-
             Randomize()
             firstRoll = Int((Rnd() * 6) + 1)
+            If firstRoll = 7 Then
+                Console.WriteLine("Error")
+                Console.ReadLine()
+            End If
             Randomize()
             secondRoll = Int((Rnd() * 6) + 1)
+            If secondRoll = 7 Then
+                Console.WriteLine("Error")
+                Console.ReadLine()
+            End If
             rollSum = firstRoll + secondRoll
 
             diceArray(rollSum, 2) += 1
@@ -23,13 +30,10 @@ Module RollOfTheDice
 
         Next
 
-        Console.WriteLine("      Total Dice    Times Rolled")
         For i = 2 To 12
             For j = 1 To 2
                 Console.Write(diceArray(i, j).ToString().PadLeft(15) & "|")
             Next
-            Console.WriteLine()
-
         Next
 
     End Sub
