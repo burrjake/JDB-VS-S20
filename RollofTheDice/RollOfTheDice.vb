8f7d9461540df5935caf3e566b3b0c8d1064dbfe
jakeb11499@gmail.com <jakeb11499@gmail.com>
2020-03-11 08:19:10 -0600

    Fix written array and add comments

--- RollofTheDice/RollOfTheDice.vb
+++ RollofTheDice/RollOfTheDice.vb
@@ -8,32 +8,31 @@ Module RollOfTheDice
         Dim diceArray(12, 2) As Integer
         For i = 1 To 1000
 
+            'Roll dice
             Randomize()
             firstRoll = Int((Rnd() * 6) + 1)
-            If firstRoll = 7 Then
-                Console.WriteLine("Error")
-                Console.ReadLine()
-            End If
             Randomize()
             secondRoll = Int((Rnd() * 6) + 1)
-            If secondRoll = 7 Then
-                Console.WriteLine("Error")
-                Console.ReadLine()
-            End If
+            'Add dice
             rollSum = firstRoll + secondRoll
 
+            'Times rolled
             diceArray(rollSum, 2) += 1
 
+            'Number between 2 and 12 rolled
             For j = 2 To 12
                 diceArray(j, 1) = j
             Next
 
         Next
 
+        'Write array
+        Console.WriteLine("   Number Rolled    Times Rolled")
         For i = 2 To 12
             For j = 1 To 2
                 Console.Write(diceArray(i, j).ToString().PadLeft(15) & "|")
             Next
+            Console.WriteLine()
         Next
 
     End Sub
